1afb05d19e02e42e7f122308e1c37c90d00e5d21
MKHAS <mha61@mail.aub.edu>
2020-12-12 11:56:16 +0200

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -41,6 +41,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,6 +57,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
